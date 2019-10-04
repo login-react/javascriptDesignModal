e573b60a6a0d8d0f242c7ae443557d551f1b4860
login-react <ztm1762530@163.com>
2019-10-04 19:40:29 +0800

    更新

--- 项目执行步骤/继承.js
+++ 项目执行步骤/继承.js
@@ -6,5 +6,5 @@
  *      3  在子类里面为 class Child extends Person
  *      4  而在了类里面则是super(name,age)这边写的是父类里面的,把这两个属性传到父类里面执行
  *      5 子类里面有一个number 属性 则this.number = number在子类里面执行
- *  adsfas
+ *
  */
